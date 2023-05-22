9bdf0a2d78760bcd92a578a34b5ada8befc4e8bd
BURAK <131662168+Tosuner123@users.noreply.github.com>
2023-05-22 14:31:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -137,6 +137,35 @@ selectGroup.forEach(group => {
   });
 });
 
+// ---- CAROUSEL1 ----
+    const gap = 50;
+    const carousel = document.getElementById("slider-carousel"),
+      content = document.getElementById("slider-content"),
+      next = document.getElementById("next"),
+      prev = document.getElementById("prev");
+    
+    next.addEventListener("click", e => {
+      carousel.scrollBy(width + gap, 0);
+      if (carousel.scrollWidth !== 0) {
+        prev.style.display = "flex";
+      }
+      if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+        next.style.display = "none";
+      }
+    });
+    prev.addEventListener("click", e => {
+      carousel.scrollBy(-(width + gap), 0);
+      if (carousel.scrollLeft - width - gap <= 0) {
+        prev.style.display = "none";
+      }
+      if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+        next.style.display = "flex";
+      }
+    });
+    
+    let width = carousel.offsetWidth;
+    window.addEventListener("resize", e => (width = carousel.offsetWidth));
+
 
 
         // ----- CAROUSEL2 -----
