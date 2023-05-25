e7548234b65367e45722349c692b28cc40c46023
BURAK <131662168+Tosuner123@users.noreply.github.com>
2023-05-25 15:34:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -204,24 +204,7 @@ selectGroup.forEach(group => {
 
 
 
-// Kopyalama işlemi için
-const kopyala = document.querySelectorAll('.fa-copy')
-kopyala.forEach(btn =>{
-  btn.addEventListener('click',(e) =>{
-    const koplanacak = e.target.parentElement.parentElement.parentElement.parentElement.children[0].children[0].textContent
-    let input = document.createElement('textarea')
-    input.type= 'text'
-    input.value= koplanacak
-    const scrollY = window.scrollY //
-    document.body.appendChild(input)
-    input.focus()
-    input.select()
-    input.setSelectionRange(0, input.value.length)
-    document.execCommand('copy')
-    document.body.removeChild(input)
-    window.scrollTo(0, scrollY) // sayfanın kaydedilen scroll pozisyonuna geri dön
-  }); 
-})
+
 
 
     
